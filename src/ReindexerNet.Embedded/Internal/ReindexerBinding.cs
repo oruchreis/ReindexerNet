5fe003e67808d26fb1c43ed60d47255fd51d6687
oruchreis <oruchreis@diyezon.com>
2020-08-05 18:35:03 +0300

    Updated Reindexer to 2.11.0

--- src/ReindexerNet.Embedded/Internal/ReindexerBinding.cs
+++ src/ReindexerNet.Embedded/Internal/ReindexerBinding.cs
@@ -23,7 +23,7 @@ namespace ReindexerNet.Embedded.Internal
 {
     internal static class ReindexerBinding
     {
-        public const string ReindexerVersion = "v2.10.0";
+        public const string ReindexerVersion = "v2.11.0";
 
         private const string BindingLibrary = "reindexer_embedded_server";
         private static class Windows
